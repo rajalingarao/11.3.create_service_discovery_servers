8b9cbd09be005121a4f4d0b5cd993908af06806d
rajalingarao4545 <rajalingarao4545@gmail.com>
2026-05-28 01:00:55 +0530

    create service discovery servers

--- node_exporter/node_exporter.sh
+++ node_exporter/node_exporter.sh
@@ -2,7 +2,8 @@
 
 USERID=$(id -u)
 TIMESTAMP=$(date +%F-%H-%M-%S)
-SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+#SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+SCRIPT_NAME=$(basename "$0" .sh)
 LOGFILE=/tmp/$SCRIPT_NAME-$TIMESTAMP.log
 R="\e[31m"
 G="\e[32m"
